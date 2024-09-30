560ca26f8d3699d36c6b5fac40cd403c2dbf6854
George Mukha <georgymukha@yandex.ru>
2024-10-01 01:15:41 +0500

    Added is_default flag and made wallet_id nullable in categories table

--- src/main/kotlin/com/themukha/smartmoney/models/Categories.kt
+++ src/main/kotlin/com/themukha/smartmoney/models/Categories.kt
@@ -8,16 +8,18 @@ import java.util.UUID
 
 object Categories : UUIDTable() {
     val name = varchar("name", 50)
-    val walletId = reference("wallet_id", Wallets)
+    val walletId = reference("wallet_id", Wallets).nullable()
     val type = enumeration("type", CategoryType::class)
+    val isDefault = bool("is_default").default(false)
 }
 
 class Category(id: EntityID<UUID>) : UUIDEntity(id) {
     companion object : UUIDEntityClass<Category>(Categories)
 
     var name by Categories.name
-    var walletId by Wallet referencedOn Categories.walletId
+    var walletId by Wallet optionalReferencedOn Categories.walletId
     var type by Categories.type
+    var isDefault by Categories.isDefault
 }
 
 enum class CategoryType {
