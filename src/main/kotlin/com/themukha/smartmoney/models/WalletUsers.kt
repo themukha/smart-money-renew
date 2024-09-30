018c23297f0a004830e589353c1800f13140e927
George Mukha <georgymukha@yandex.ru>
2024-10-01 02:06:47 +0500

    Fixed wallet_users table name

--- src/main/kotlin/com/themukha/smartmoney/models/WalletUsers.kt
+++ src/main/kotlin/com/themukha/smartmoney/models/WalletUsers.kt
@@ -2,7 +2,7 @@ package com.themukha.smartmoney.models
 
 import org.jetbrains.exposed.sql.Table
 
-object WalletUsers : Table() {
+object WalletUsers : Table("wallet_users") {
     val userId = reference("user_id", Users)
     val walletId = reference("wallet_id", Wallets)
     val role = enumerationByName("role", 8, UserRole::class).default(UserRole.READER)
